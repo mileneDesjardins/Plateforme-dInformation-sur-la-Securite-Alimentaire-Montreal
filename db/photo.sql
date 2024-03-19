8a3de286fed4fa314233f5d5ccbcd110489b8d1d
Milene Desjardins <m.desjardins31@outlook.com>
2024-03-19 10:18:46 -0400

    ajout tables

---
+++ db/photo.sql
@@ -0,0 +1,4 @@
+CREATE TABLE PHOTO (
+    id_photo VARCHAR(255) PRIMARY KEY,
+    data BLOB NOT NULL
+)
---
+++ db/user.sql
@@ -0,0 +1,15 @@
+CREATE TABLE User(
+    id_user INTEGER NOT NULL PRIMARY KEY,
+    prenom varchar(25) NOT NULL,
+    nom varchar(25) NOT NULL,
+    courriel varchar(100) NOT NULL,
+    choix_etablissements INTEGER[] NOT NULL,
+    mdp_hash varchar(128) NOT NULL,
+    mdp_salt varchar(32) NOT NULL,
+    id_photo VARCHAR(255),
+    CONSTRAINT fk_photo
+        FOREIGN KEY (id_photo)
+        REFERENCES photo(id_photo)
+
+
+)
