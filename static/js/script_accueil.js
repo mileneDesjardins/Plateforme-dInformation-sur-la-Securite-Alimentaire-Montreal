881d4cd9ac5d2d83db58b7ca2fa1d42e4d6e0737
Anne-Sophie Abel-Levesque <fj091076@ens.uqam.ca>
2024-03-24 12:50:17 -0400

    Enlever fonction inutilisees

--- static/js/script_accueil.js
+++ static/js/script_accueil.js
@@ -74,65 +74,6 @@ function countContraventions(contraventions) {
 }
 
 
-// Creer la table HTML
-function creerTable(data, resultAffiche) {
-
-    const table = document.createElement("table");
-    table.classList.add("table", "table-striped", "table-bordered", "table-sm");
-    const thead = creerEnteteTable();
-    const tbody = creerCorpsTable(data);
-    table.appendChild(thead);
-    table.appendChild(tbody);
-    resultAffiche.appendChild(table);
-}
-
-// Creer en-tete de la table
-function creerEnteteTable() {
-    const thead = document.createElement("thead");
-    const headerRow = document.createElement("tr");
-
-    const th1 = creerCelluleEntete("Établissement");
-    const th2 = creerCelluleEntete("Nombre de contraventions");
-
-    headerRow.appendChild(th1);
-    headerRow.appendChild(th2);
-    thead.appendChild(headerRow);
-
-    return thead;
-}
-
-// Creer corps de la table
-function creerCorpsTable(data) {
-    const tbody = document.createElement("tbody");
-
-    // Appel de la fonction countContraventions avec les données JSON
-    const resultTable = countContraventions(data);
-
-    // Remplir les données de la table avec les résultats de countContraventions
-    for (const etablissement in resultTable) {
-        const row = document.createElement("tr");
-        const td1 = creerCelluleDonnee(etablissement);
-        const td2 = creerCelluleDonnee(resultTable[etablissement]);
-
-        row.appendChild(td1);
-        row.appendChild(td2);
-        tbody.appendChild(row);
-    }
-
-    return tbody;
-}
-
-function creerCelluleEntete(texte) {
-    const th = document.createElement("th");
-    th.textContent = texte;
-    return th;
-}
-
-function creerCelluleDonnee(texte) {
-    const td = document.createElement("td");
-    td.textContent = texte;
-    return td;
-}
 
 
 document.getElementById('btn-fast-search').addEventListener("click", onFastSearchSubmit);
