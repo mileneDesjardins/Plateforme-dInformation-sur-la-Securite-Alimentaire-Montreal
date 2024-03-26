662ae74e752f4fda5e74431a2e1227f23d8a27f3
Milene Desjardins <m.desjardins31@outlook.com>
2024-03-26 13:26:14 -0400

    afficher liste avec etablissement selected au compte du user

--- app.py
+++ app.py
@@ -251,37 +251,28 @@ def compte():
     elif request.method == 'POST':
 
         # Récupérer les informations soumises dans le formulaire
-
         new_etablissements = request.form.getlist('choix_etablissements')
 
         # Mettre à jour les établissements sélectionnés dans la base de données
-
         db.update_user_etablissements(id_user, new_etablissements)
 
         # Mettre à jour les établissements sélectionnés dans la session
-
-        session["choix_etablissements"] = json.dumps(new_etablissements)
+        session["choix_etablissements"] = new_etablissements
 
         nouvelle_photo = request.files.get('photo')
 
         # Mettre à jour les établissements choisis pour l'utilisateur
-
         if new_etablissements:
             db.update_user_etablissements(id_user, new_etablissements)
-
-            session["choix_etablissements"] = json.dumps(new_etablissements)
+            session["choix_etablissements"] = new_etablissements
 
         # Enregistrer la nouvelle photo dans la base de données et mettre à jour l'ID de la photo de l'utilisateur
-
         if nouvelle_photo is not None:
             photo_data = nouvelle_photo.read()
-
             id_photo = db.create_photo(photo_data)
-
             db.update_user_photo(id_user, id_photo)
 
         # Rediriger vers la page de confirmation des modifications de l'utilisateur
-
         return redirect(url_for('confirmation_modifs_user'))
 
 @app.route('/photo/<id_photo>')
