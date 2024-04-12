2ccccc00b4c87d5abe66dd152de3a7b04de4f28f
Milene Desjardins <m.desjardins31@outlook.com>
2024-04-12 13:20:36 -0400

    .env_example

--- sync_database.py
+++ sync_database.py
@@ -5,6 +5,8 @@ import os
 
 from database import Database
 
+# TODO EST CE QUON GARDE CE SCRIPT?
+
 db = Database()
 url = ('https://data.montreal.ca/dataset/05a9e718-6810-4e73-8bb9-5955efeb91a0/'
        'resource/7f939a08-be8a-45e1-b208-d8744dca8fc6/download/violations.csv')
